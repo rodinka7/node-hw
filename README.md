5016c1121f8987dccc9b0c160246cbe1ce282d9d
taniadan <taniadan2402@gmail.com>
2019-12-23 23:46:08 +0200

    Homework 2 server with fixes

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Loftschool NodeJS Homeworks
+
+Homework 2. Http Server
+node index.js --interval [Number] --duration [Number]
--- src/showTime.js
+++ src/showTime.js
@@ -1,33 +1,31 @@
-module.exports = ({ INTERVAL, DURATION }, server) => {
-    if (!INTERVAL || !DURATION) return;
-
-    const interval = +INTERVAL*1000;
-    const duration = +DURATION*1000;
-
-    const begin = new Date();
-    const end = begin.getTime() + duration;
-
-    process
-        .stdout
-        .write(`${begin.toUTCString()} \n`);
-        
-    const timer = setInterval(() => {
-        const date = new Date();
-        const utcDate = date.toUTCString();
-        const stop = date.getTime() + interval >= end;
-
-        const str = stop 
-            ? `Вывод в консоль остановлен в ${utcDate} \n`
-            : `${utcDate} \n`;
-
-        process
-            .stdout
-            .write(str);
-        
-        if (stop){
-            clearInterval(timer);
-            process.exit(0);
-        } 
-    }, interval);
+module.exports = (interval, duration) => {
+    return new Promise((response, reject) => {
+        if (!interval || !duration) reject();
+    
+        interval *= 1000;
+        duration *= 1000;
+    
+        const begin = new Date();
+        const end = begin.getTime() + duration;
+    
+        console.log(`${begin.toUTCString()} \n`);
+            
+        const timer = setInterval(() => {
+            const date = new Date();
+            const utcDate = date.toUTCString();
+            const stop = date.getTime() + interval >= end;
+    
+            const str = stop 
+                ? `Вывод в консоль остановлен в ${utcDate} \n`
+                : `${utcDate} \n`;
+    
+            if (!stop){
+                console.log(str);
+            } else {
+                clearInterval(timer);
+                response(str);                
+            }
+        }, interval);
+    })
     
 }
